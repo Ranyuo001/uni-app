7aa8ac65cd52dc9b7f930c23f6bd2135e9f6eda2
ranyuo <ranyuo002@163.com>
2025-02-16 23:29:52 +0800

    进行了请求函数封装

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
 import '@/utils/http'
+import { http } from '@/utils/http'
 
 const memberStore = useMemberStore()
 
-const getData = () => {
-  uni.request({
+const getData = async () => {
+  const res = await http({
     method: 'GET',
     url: '/home/banner',
     header: {},
   })
+  console.log(res)
 }
 </script>
 
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -41,3 +41,40 @@ const httpInterceptor = {
 // 修正拼写错误
 uni.addInterceptor('request', httpInterceptor)
 uni.addInterceptor('uploadFile', httpInterceptor)
+
+/* 封装请求方法
+@param UniApp.RequestOptions
+返回Promise
+@eturns Promise
+1.返回Promise对象
+2.请求成功
+  2.1提取核心数据res.data
+  2.2添加类型，支持泛型
+3.请求失败
+  3.1网络错误->提示用户换网络
+  3.2其他错误->根据后端错误信息轻提示
+  3.3网络错误、其他错误->统一提示
+
+*/
+
+interface Data<T> {
+  code: string
+  msg: string
+  result: T
+}
+
+// 确保这里正确导出 http 函数
+//添加类型，支持泛型
+export const http = <T>(options: UniApp.RequestOptions) => {
+  return new Promise<Data<T>>((resolve, reject) => {
+    uni.request({
+      ...options,
+      success: (res) => {
+        resolve(res.data as Data<T>)
+      },
+      fail: (err) => {
+        reject(err)
+      },
+    })
+  })
+}
